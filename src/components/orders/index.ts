4b386c745f23686ed7d2036b6895d48090842c32
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2025-07-17 20:19:14 -0300

    dados

---
+++ src/components/orders/index.ts
@@ -0,0 +1,10 @@
+// orders.ts
+export const orders = [
+  { id: 1, product_id: 1, quantity: 3, client_id: 1 },
+  { id: 2, product_id: 2, quantity: 12, client_id: 2 },
+  { id: 3, product_id: 2, quantity: 10, client_id: 3 },
+  { id: 4, product_id: 4, quantity: 2, client_id: 3 },
+  { id: 5, product_id: 1, quantity: 1, client_id: 3 },
+  { id: 6, product_id: 1, quantity: 5, client_id: 6 },
+  { id: 7, product_id: 4, quantity: 2, client_id: 5 },
+];
---
+++ src/components/products/index.ts
@@ -0,0 +1,8 @@
+// products.ts
+export const products = [
+  { id: 1, name: 'Ração Pedigree', price: 80 },
+  { id: 2, name: 'Ovo', price: 1 },
+  { id: 3, name: 'Bolinha Cachorro', price: 10 },
+  { id: 4, name: 'Areia gato', price: 35 },
+  { id: 5, name: 'Bala', price: 0.5 },
+];
